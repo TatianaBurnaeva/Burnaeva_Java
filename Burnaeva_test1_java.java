32bbd1c449e1967cd2309faaf8c0de62767fb584
TatianaBurnaeva <potraz325@gmail.com>
2024-11-29 20:03:08 +0400

    Update Burnaeva_test1_java.java
    Добавила проверку деления на 0

--- Burnaeva_test1_java.java
+++ Burnaeva_test1_java.java
@@ -7,16 +7,20 @@ public class Main {
     int a = scanner.nextInt();
     System.out.println("введи число b");
     int b = scanner.nextInt();
-        int sum = a+b;
-        int vychet = a-b;
-        int umnog = a*b;
-        double del = (double)a/b;
-  if(a > b) {
+    int sum = a+b;
+    int vychet = a-b;
+    int umnog = a*b;
+    double del = (double)a/b;
+  if(a > b && b != 0) {
     System.out.println("a > b "+", сумма="+sum+", вычитание="+vychet+", умножение="+umnog+" , деление="+del);
-  } if(a < b) {
+  } if(a < b && b != 0) {
     System.out.println("a < b"+", сумма="+sum+", вычитание="+vychet+", умножение="+umnog+" , деление="+del);
-    } if (a == b) {
+    } if (a == b && b != 0) {
   System.out.println("a = b"+", сумма="+sum+", вычитание="+vychet+", умножение="+umnog+" , деление="+del);
+    }
+     if (b == 0)  {
+        System.out.println("на ноль делить нельзя");
+    System.out.println("сумма="+sum+", вычитание="+vychet+", умножение="+umnog);
     }
 }
 }
